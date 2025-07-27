e6296e733d102015b3e97651e1ffb14d878bb214
aukseeskua <aukse.kepalaite@gmail.com>
2025-07-27 16:53:33 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
-
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration'; // <- Pridėta
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
+
+// Užregistruojam PWA service worker
+serviceWorkerRegistration.register(); // <- Pridėta
